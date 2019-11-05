2d488924ed799885cf5b5063cbdb33547401aeea
Agbele_Kolade <kolagbele@gmail.com>
2019-11-05 06:00:05 +0100

    updated server endpoint with liveserver url

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({location}) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://kaeyzchatapp-server.herokuapp.com/";
 
 
    useEffect(() => {
