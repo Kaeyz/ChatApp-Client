f7b7d2cf04197914f3e3df2b00b2d77a61fd4c7a
Agbele_Kolade <kolagbele@gmail.com>
2019-11-05 06:06:50 +0100

    updates placeholder for join form

--- src/components/join/Join.js
+++ src/components/join/Join.js
@@ -13,7 +13,7 @@ const Join = () => {
         <h1 className="heading">Join</h1>
         <div>
           <input
-            placeholder=""
+            placeholder="enter username"
             className="joinInput"
             type="text"
             onChange={(event => setName(event.target.value))}
@@ -21,7 +21,7 @@ const Join = () => {
         </div>
         <div>
           <input
-            placeholder=""
+            placeholder="enter chat room"
             className="joinInput mt-20"
             type="text"
             onChange={(event => setRoom(event.target.value))}
